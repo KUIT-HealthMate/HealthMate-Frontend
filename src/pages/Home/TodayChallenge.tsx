06c140cd7282d7d0d9ffd0c3ccb697598f932abf
dainshon <shon1107@naver.com>
2024-07-15 19:04:20 +0900

    Feat(#1): 오늘의 챌린지 0일때 조건부 추가

--- src/pages/Home/TodayChallenge.tsx
+++ src/pages/Home/TodayChallenge.tsx
@@ -5,10 +5,6 @@ import goodIcon from "../../assets/good.svg";
 import badIcon from "../../assets/bad.svg";
 import bubleGreen from "../../assets/bubleG.svg";
 import bubleOrange from "../../assets/bubleO.svg";
-import styled from "styled-components";
-
-// const StyledChart = styled.input`
-// `;
 
 
 export default function TodaysChallenge() {
@@ -37,15 +33,20 @@ export default function TodaysChallenge() {
                     } : {
                         background: `conic-gradient(#F5BE9D 0%, #F7A682 ${halfPercent}%, #F97F59 ${percent}%, #D9D9D9 ${percent}% 100%)`
                     }}>
-                        <div className={styles.ChartCenter}><p className={styles.PercentText} data-percent={percent} style={percent >= 50 ?
-                            { color: '#0E9494' } : { color: '#F8825C' }}></p></div>
+                        <div className={styles.ChartCenter}>
+                            <p className={styles.PercentText} data-percent={percent}
+                                style={percent >= 50 ? { color: '#0E9494' } : { color: percent > 0 ? '#F8825C' : '#8F8F8F' }}>
+                            </p>
+                        </div>
+
                     </div>
                 </div>
+                {/* 목표치 달성시 멘트 추가수정해야됨 */}
                 <div className={styles.ChallengeState} style={percent >= 50 ?
-                    { background: 'rgba(122, 220, 197, 0.25)' } : { background: 'rgba(249, 129, 91, 0.2)' }}>
-                    <p className={styles.ChallengeStateText} style={percent >= 50 ? { color: '#0B7575' } : { color: '#F8825C' }}>
-                        {percent >= 50 ? '거의 다 진행하셨네요!' : '조금만 더 힘을 내볼까요?'}
-                        <img src={percent >= 50 ? goodIcon : badIcon}></img>
+                    { background: 'rgba(122, 220, 197, 0.25)' } : { background: percent > 0 ? 'rgba(249, 129, 91, 0.2)' : '#EBEBEB' }}>
+                    <p className={styles.ChallengeStateText} style={percent >= 50 ? { color: '#0B7575' } : { color: percent > 0 ? '#F8825C' : '#8F8F8F' }}>
+                        {percent >= 50 ? '거의 다 진행하셨네요!' : percent > 0 ? '조금만 더 힘을 내볼까요?' : '건강 챌린지를 추가해보세요!'}
+                        <img src={percent >= 50 ? goodIcon : percent > 0 ? badIcon : ''}></img>
                     </p>
                 </div>
                 <button className={styles.StatisticsButton}><p className={styles.StatisticsButtonText}>전체 챌린지 통계 보기</p></button>
