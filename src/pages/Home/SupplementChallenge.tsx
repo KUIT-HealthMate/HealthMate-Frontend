91b37e5235639f4550e076f0d971ef4af88fcc86
dainshon <shon1107@naver.com>
2024-07-16 02:20:58 +0900

    Feat(#1): 영양제 챌린지 detail

--- src/pages/Home/SupplementChallenge.tsx
+++ src/pages/Home/SupplementChallenge.tsx
@@ -1,17 +1,60 @@
 import styles from "./Home.module.scss";
-import pill from "../../assets/pill.svg";
+import pillIcon from "../../assets/pill.svg";
 import clampR from "../../assets/clampR.svg";
+import React, { useEffect, useState } from 'react';
 
 
-const SupplementChallenge = () => (
-    <div className={styles.PillChallenge}>
-        <div className={styles.PillChallengeTitle}>
-            <img src={pill} className={styles.PillImg}></img>
-            <h1 className={styles.PillText}>영양제 챌린지</h1>
-            <h3 className={styles.PillEdit}>편집하기<img src={clampR} className={styles.clampR}></img></h3>
-        </div>
 
-    </div>
-)
+//const SupplementChallenge = () => (
 
-export default SupplementChallenge;
+export default function SupplementChallenge() {
+
+    const [pillInfo, setPillInfo] = useState([
+        { id: 1, name: '베아제', info: "식후 30분 이내", times: ["아침", "점심", "저녁"] },
+        { id: 2, name: '루테인', info: "식후 1시간 이내", times: ["아침", "점심", "저녁"] }
+    ]);
+
+    return (
+
+        < div className={styles.PillChallenge} >
+            <div className={styles.PillChallengeTitle}>
+                <img src={pillIcon} className={styles.PillImg}></img>
+                <h1 className={styles.PillText}>영양제 챌린지</h1>
+                <h3 className={styles.PillEdit}>편집하기<img src={clampR} className={styles.clampR}></img></h3>
+            </div>
+
+            <div>
+                {
+                    pillInfo.map((pill, index) => {
+                        return (
+                            <div className={styles.PillInfo}>
+                                <div className={styles.PillInfoHeader}>
+                                    <h1 className={styles.PillInfoName}>{pill.name}</h1> <h5 className={styles.PillInfoInfo}>{pill.info}</h5>
+                                </div>
+                                <div>
+                                    <div className={styles.PillInfoTimes}>
+                                        {
+                                            pill.times.map((time, idx) => {
+                                                return (
+
+                                                    <div className={styles.PillInfoTimeButton}>{time}</div>
+
+                                                )
+
+                                            })
+                                        }
+                                    </div>
+                                </div>
+                            </div >
+                        )
+                    })
+                }
+            </div>
+
+
+
+
+        </div >)
+};
+
+//export default SupplementChallenge;
