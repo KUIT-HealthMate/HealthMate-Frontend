d89dfb02ee29d52a1219e6dcac0af2521dbdf958
dainshon <shon1107@naver.com>
2024-07-17 10:29:06 +0900

    Fix(#1): merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
-import React from "react";
-import BottomBar from "./components/organs/BottomBar";
-import TopBar from "./components/organs/TopBar";
 
-function App() {
-  return (
-    <div className="App">
-      <TopBar />
-      <BottomBar />
-      <header className="App-header"></header>
-    </div>
-=======
 import './App.css';
 import Router from './Router';
 
 function App() {
   return (
     <Router />
->>>>>>> 70b4ccf (Feat: 리액트 파일 구구조)
+
   );
 }
 
