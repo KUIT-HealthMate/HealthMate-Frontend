5d8d1ca93a75127ba80aae54c2316832ce766b73
seungwoo <2002msw@naver.com>
2024-08-03 02:13:41 +0900

    Feat: 로그인 약관 모달창 일부 구현

--- src/pages/login/EmailCheckPage.tsx
+++ src/pages/login/EmailCheckPage.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { useGlobalStore } from '../../store/store';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 import s from "./EmailCheckPage.module.scss"
+import TermsPage from './TermsPage';
+
+import kakaoTalkIconWithBackground from "../../assets/loginPage/kakakTalkIconWithBackground.svg";
+import leftBraket from "../../assets/leftBraket.svg";
 
 const EmailCheckPage = () => {
   const { setShowBottomBar } = useGlobalStore();
+  const navigate = useNavigate();
 
   useEffect(() => {
     setShowBottomBar();
@@ -14,23 +21,37 @@ const EmailCheckPage = () => {
     };
   },[])
 
+  // 서버에서 이메일 받아서 설정
+  const handleEmailName = () => {
+    const span = document.getElementsByClassName(s.emailText)[0] as HTMLSpanElement;
+    span.innerText = "";
+  }
+
+  const handleBackbutton = () => {
+    navigate(-1);
+  }
+
+  // 약관 모달창 state값. true 시 모달창이 표시됨.
+  const [modal, setModal] = useState(false);
+
   return (
-    <>
-      <div>
-        <button type="button">
-          <img src="" alt="" />
+    <div className={s.pageWrap}>
+      <div className={s.emailCheckSection}>
+        <button type="button" onClick={() => handleBackbutton()}>
+          <img className={s.leftBraketImg} src={leftBraket} alt="" />
         </button>
-        <span>이메일을 확인해주세요</span>
-        <div>
-          <img src="" alt="" />
-          <span>user@example.com</span>
+        <span className={s.emailCheckText}>이메일을 확인해주세요</span>
+        <div className={s.emailWrap}>
+          <img src={kakaoTalkIconWithBackground} alt="" />
+          <span className={s.emailText}>user@example.com</span>
         </div>
       </div>
-      <button type="button">
+      <button className={s.emailNextButton} type="button" onClick={() => setModal(true)}>
         다음
       </button>
-    </>
-  )
+      {modal === true ? <TermsPage modal={modal} setModal={setModal}/> : null}
+    </div>
+  );
 }
 
 export default EmailCheckPage
--- src/pages/login/TermsPage.tsx
+++ src/pages/login/TermsPage.tsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import { useRef } from 'react';
+import s from "./TermsPage.module.scss";
 
-const TermsPage = () => {
+import uncheckedCheckBox from "../../assets/loginPage/uncheckedCheckbox.svg";
+import checkedCheckBox from "../../assets/loginPage/checkedCheckbox.svg";
+import rightBraket from "../../assets/loginPage/rightBraket.svg";
+
+interface Props {
+  modal: boolean;
+  setModal: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const TermsPage: React.FC<Props> = ({modal, setModal}) => {
+
+
+  const modalBackground = useRef();
+
+  
+
+  const handleTimeSet = () => {
+    setModal(false);
+    //handleAlarmTime();
+  }
+
   return (
-    <div>TermsPage</div>
+    <div className={s.AlarmTimeInputWrapper} ref={modalBackground as any} onClick={e => {
+        if(e.target === modalBackground.current) {
+            setModal(false);
+        }
+    }}>
+        <div className={s.AlarmTimeInputContent}>
+          <div className={s.TitleBar}>
+            <span className={s.AgreeTermsTitle}>약관에 동의해 주세요</span>
+          </div>
+          <div className={s.TermsDetailWrap}>
+            <div className={s.agreeAllTerms}>
+              <img src={uncheckedCheckBox} alt="Unchecked" />
+              <img src={checkedCheckBox} alt="Checked" />
+              <span>약관 전체동의</span>
+            </div>
+            <div className={s.eachTermWrap}>
+              <img src={uncheckedCheckBox} alt="Unchecked" />
+              <img src={checkedCheckBox} alt="Checked" />
+              <span>서비스 이용약관 전체동의(필수)</span>
+              <button type="button"><img src={rightBraket} alt="" /></button>
+            </div>
+            <div className={s.eachTermWrap}>
+              <img src={uncheckedCheckBox} alt="Unchecked" />
+              <img src={checkedCheckBox} alt="Checked" />
+              <span>개인 정보 수집 및 이용 동의(필수)</span>
+              <button type="button"><img src={rightBraket} alt="" /></button>
+            </div>
+          </div>
+          <button type="button" className={s.agreeTermsCompleteButton} onClick={() => handleTimeSet()}>확인</button>
+        </div>
+    </div>
   )
 }
 
