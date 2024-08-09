2924b049137563cc316f1dc4026c122ce0f5ff15
seungwoo1209 <2002msw@naver.com>
2024-08-10 07:17:51 +0900

    Feat: getRequestBodyFromChallenge 파일 분리

--- src/APIs/registerPill.ts
+++ src/APIs/registerPill.ts
@@ -1,5 +1,6 @@
 import habitInfo from "../store/habitInfo";
 import pillInfo from "../store/pillInfo";
+import { getRequestBodyFromChallenge } from "./getRequestBodyFromChallenge";
 import { tokenAsString } from "./tokenAsString";
 
 let header = new Headers();
@@ -24,67 +25,3 @@ export const registerPill = (pillToRegister : pillInfo) => {
   // .catch(error => console.log('registerPill Error', error));
 }
 
-const getRequestBodyFromChallenge = <T extends pillInfo | habitInfo>(challengeToRegister: T):string => {
-  
-  let result:string = "{\n  " + stringifyObjects(challengeToRegister) + "\n}";
-
-  console.log("getRequestBodyFromChallenge: "+result);
-  return result;
-
-}
-
-const isObject = (value: any): boolean => {
-  return value !== null && typeof value === 'object' && !Array.isArray(value);
-}
-
-const isArray = (value: any): boolean => {
-  return Array.isArray(value);
-}
-
-const stringifyObjects = (inputObj: object):string => {
-  let result:string = "";
-  let arrayOfKeysAndValues = Object.entries(inputObj);
-  arrayOfKeysAndValues.map(([key, value], index) => {
-    result += `\"${key}\": `;
-    if(isObject(value)){
-      result += "{\n    ";
-      result += stringifyObjects(value);
-      result += `}${isLastElement(arrayOfKeysAndValues,index) ? "" : ","}\n  `;
-    } else if(isArray(value)){
-      result += "[\n    ";
-      result += stringifyArrays(value);
-      result += "]\n";
-    } else {
-      result += `\"${value}\"${isLastElement(arrayOfKeysAndValues,index) ? "" : ","}\n  `
-      console.log(value);
-    }
-  });
-
-  return result;
-}
-
-
-const stringifyArrays = (inputArr: object):string => {
-  let result:string = "";
-  let arrayOfKeysAndValues = Object.entries(inputArr);
-  arrayOfKeysAndValues.map(([key, value], index) => {
-    if(isObject(value)){
-      result += "{\n    ";
-      result += stringifyObjects(value);
-      result += `}${isLastElement(arrayOfKeysAndValues,index) ? "" : ","}\n  `;
-    } else if(isArray(value)){
-      result += "[\n    ";
-      result += stringifyArrays(value);
-      result += "]\n";
-    } else {
-      result += `\"${value}\"${isLastElement(arrayOfKeysAndValues,index) ? "" : ","}\n  `
-      console.log(value);
-    }
-  });
-
-  return result;
-}
-
-const isLastElement = (array: [string, any][], index: number):boolean => {
-  return index === (array.length - 1);
-}
