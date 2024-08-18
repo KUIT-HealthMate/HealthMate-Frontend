281782141824b11dbc5e4ce5e7e328c30f1eb1b9
seungwoo <2002msw@naver.com>
2024-08-18 15:13:09 +0900

    Fix: eslint warning resolved

--- src/pages/Home/Challenge/ChallengeEditingPage.tsx
+++ src/pages/Home/Challenge/ChallengeEditingPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { usePillInfoStore } from "../../../store/usePillInfoStore";
 import { useEffect } from "react";
 import { useGlobalStore } from "../../../store/store";
